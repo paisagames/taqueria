29b92e754f3cb7ad9c74304c77fba10b8980a67e
juanregules <33490910+juanregules@users.noreply.github.com>
2018-10-15 13:40:58 -0700

    prototipo_1

---
+++ Assets/scripts/canvascode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvascode : MonoBehaviour {
+	public RectTransform canvass;
+	public Image imagenbase;
+	float canvas_X;
+	float canvas_Y;
+	// Use this for initialization
+	void Start () {
+		canvas_X = canvass.sizeDelta.x;
+		canvas_Y = canvass.sizeDelta.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		canvas_X = canvass.sizeDelta.x;
+		canvas_Y = canvass.sizeDelta.y;
+
+		imagenbase.rectTransform.localScale=new Vector3(canvas_X/100,canvas_Y/100,0);
+		imagenbase.rectTransform.position = new Vector3 (0, 0, 0);
+
+		
+	}
+
+	//poner la imagen base
+	//poner la imagen ganado
+
+	/*
+KEYSTORE:
+c:/9 semestre/Rolling 15/Rolling.keystore
+
+	*/
+}
---
+++ Assets/scripts/movefood.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movefood : MonoBehaviour {
+	public Transform pos1;
+	public Transform pos2;
+	public Transform pos3;
+	public Transform coca;
+	public Transform banana;
+	public Transform cafe;
+	int num;
+
+	// Use this for initialization
+	void Start () {
+		num=1;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void NEXT(){
+		num++;
+		if(num>=4){num=1;}
+		changefood();
+	}
+	public void BEFORE(){
+		num--;
+		if(num<=0){
+			num=3;
+		}
+		changefood();
+	}
+	public void changefood(){
+		switch(num){
+			case 1:coca.position=pos1.position;
+					banana.position=pos2.position;
+					cafe.position=pos3.position;break;
+			case 2:coca.position=pos2.position;
+					banana.position=pos3.position;
+					cafe.position=pos1.position;break;
+
+			case 3:coca.position=pos3.position;
+					banana.position=pos1.position;
+					cafe.position=pos2.position;break;
+		}
+	}
+}
